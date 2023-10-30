fc2c0d615ed68750e3564fbcac087fd371ebb6ec
Francesco-Munafo <acquamarea1@gmail.com>
2023-10-30 15:30:38 +0100

    Add data to seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,14 +4,26 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\train;
 
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+
+            $train = new Train();
+            $train->agency = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->dateTimeBetween('now', '+1 week');
+            $train->arrival_time = $faker->dateTimeBetween('now', '+1 week');
+            $train->train_code = $faker->bothify('??-#########');
+            $train->wagons_number = $faker->randomNumber(2, false);
+        }
     }
 }
